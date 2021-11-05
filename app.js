d9d0e94ae51b3ba3a55a943a6905ce6cb2a101f8
Emily Landers <emily.j.landers@gmail.com>
2021-11-04 23:21:31 -0400

    lab 9

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 'use strict';
 
 let hours = ['6:00am', '7:00am', '8:00am', '9:00am', '10:00am', '11:00am', '12:00pm', '1:00pm', '2:00pm', '3:00pm', '4:00pm', '5:00pm', '6:00pm', '7:00pm'];
-
+let storeFormEl = document.getElementById('addStore');
 function generateRange(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
@@ -20,27 +20,30 @@ function Store(name, minCustPerHour, maxCustPerHour, avgCookPerCust){
       let customerSales = Math.round(numOfCust * this.avgCookPerCust);
       this.salesTotals.push(` ${customerSales}`);
       this.grandTotal = this.grandTotal + customerSales;
+
     }
-  }
-    this.render = function(){
-      this.hourlyCustomers()
-      let body = document.getElementById('tbody')
-      let row = document.createElement('tr')
-      row.textContent = this.name;
-      body.appendChild(row)
-    for (let i = 0; i < hours.length; i++){
-      let saleTotal = document.createElement('td') 
-      saleTotal.textContent = this.salesTotals[i]
-      row.appendChild(saleTotal);
-    }
-    let dailyTotal = document.createElement('td')
-    dailyTotal.textContent = this.grandTotal
-    row.appendChild(dailyTotal)
   }
 }
 Store.prototype.generateHourlySales = function () {
 
 }
+
+  Store.prototype.render = function(){
+    this.hourlyCustomers()
+    let body = document.getElementById('tbody')
+    let row = document.createElement('tr')
+    row.textContent = this.name;
+    body.appendChild(row)
+  for (let i = 0; i < hours.length; i++){
+    let saleTotal = document.createElement('td') 
+    saleTotal.textContent = this.salesTotals[i]
+    row.appendChild(saleTotal);
+  }
+  let dailyTotal = document.createElement('td')
+  dailyTotal.textContent = this.grandTotal
+  row.appendChild(dailyTotal)
+}
+
 
 function tableHeader(){
   let header = document.getElementById('thead');
@@ -61,7 +64,7 @@ function tableFooter(){
   let title = document.createElement('tf');
   title.textContent = "Hourly Totals"
   footer.appendChild(title)
-  for(let i = 0; i < this.salesTotals.length; i ++){
+  for(let i = 0; i < hours.length; i ++){
   let hrTotals = document.createElement('tf')
   hrTotals.textContent = this.hourlyTotals();
   footer.appendChild(hrTotals)
@@ -85,4 +88,27 @@ let paris = new Store ('Paris', 20, 38, 2.3)
 let lima = new Store('Lima', 2, 16, 4.6)
   lima.render()
 
-  tableFooter()
+  Store.prototype.averageCookiesPurchased = function () {
+    for (let i = 0; i < Hours.length; i++) {
+        let cookieSales = Math.floor(generateRange(this.min_customers, this.max_customers) * this.average_cookies);
+        this.salesTotals.push(cookieSales);
+        this.grandTotal = this.grandTotal + cookieSales;
+    }
+};
+
+  function generateStore (formSubmission) {
+    formSubmission.preventDefault();
+    let name = formSubmission.target.storeName.value;
+    let min_cus = formSubmission.target.mincus.value;
+    let max_cus = formSubmission.target.maxcus.value;
+    let average_cook = formSubmission.target.avgcookie.value;
+    let newMinCus = parseInt(min_cus);
+    let newMaxCus = parseInt(max_cus);
+    let newAvgDaily = parseInt(average_cook);
+
+    let franchise = new Store(name, newMinCus, newMaxCus, newAvgDaily);
+
+    franchise.render();
+    tableFooter()
+}
+storeFormEl.addEventListener('submit', generateStore);
